2aa57c428756cd0b04004fbf1b6b323a9ff3641d
owned <owned.by.dz@gmail.com>
2019-01-16 09:05:55 +0200

    some tests

--- src/test/java/lv/lottery/lottery/LotteryControllerTest.java
+++ src/test/java/lv/lottery/lottery/LotteryControllerTest.java
@@ -2,6 +2,8 @@ package lv.lottery.lottery;
 
 import lv.lottery.registration.LotteryController;
 import lv.lottery.registration.LotteryRegistration;
+import lv.lottery.users.UserController;
+import lv.lottery.users.UsersRegistration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ public class LotteryControllerTest {
     @Autowired
     private LotteryController lotteryController;
 
+    @Autowired
+    private UserController userController;
+
     @Test
     public void shouldCreateLottery(){
         LotteryRegistration input = new LotteryRegistration();
@@ -36,4 +41,42 @@ public class LotteryControllerTest {
 
 
     }
+
+    @Test
+    public void shouldStopLottery(){
+        LotteryRegistration input = new LotteryRegistration();
+
+        input.setTitle("Test Lottery");
+        input.setLimit(10);
+        input.setRegStatus(true);
+
+        lotteryController.create(input);
+
+
+        UsersRegistration inputUser = new UsersRegistration();
+
+        inputUser.setAge((byte) 22);
+        inputUser.setEmail("test2@test.com");
+        inputUser.setAssignedLotteryId(1L);
+        inputUser.setCode("1601191400000001");
+
+        userController.add(inputUser);
+
+
+
+        lotteryController.stopLotReg(input);
+
+
+
+        Optional<LotteryRegistration> lottery = lotteryController.getById(1L);
+
+        assertThat(lottery).isPresent();
+        assertThat(lottery.get().getTitle()).isEqualTo("Test Lottery");
+        assertThat(lottery.get().getLimit()).isEqualTo(10);
+        assertThat(lottery.get().getRegStatus()).isEqualTo(false);
+
+
+    }
+
+
 }
